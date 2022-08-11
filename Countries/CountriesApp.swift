fc2998dcb6cb343ea22bd8fda5141d83ead25799
Carl Baltrunas <carl@reststop.com>
2022-08-11 16:56:11 -0700

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by carl on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
